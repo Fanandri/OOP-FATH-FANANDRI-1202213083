ba03c91cd17d41a9793cfb35092bb5a78d06749b
Fanandri <102351493+Fanandri@users.noreply.github.com>
2022-11-04 16:49:48 +0700

    modul1

---
+++ MODUL1/Server.java
@@ -0,0 +1,22 @@
+package MODUL1;
+
+import java.util.Scanner;
+
+public class Server {
+
+    public static void main(String[] args) {
+        Scanner  
+        // TODO Create Database
+        public void createDatabase()
+        // TODO Create Menu
+        public void createmenu()
+        // TODO Insert Menu to Database
+        public void insertMenu()
+
+        // TODO Display Main Menu
+
+        // TODO Create User
+
+        // Display Menu
+    }
+}
